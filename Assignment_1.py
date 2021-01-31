6db6b473ecf5a159bb5b09255b5bd1e11bb4273c
LongHoang02 <71079464+LongHoang02@users.noreply.github.com>
2021-01-31 14:21:58 +0100

    Create Assignment_1.py

---
+++ Assignment_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# About: This Notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np 
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
